cc2b30e5bf60dd5d3dcf6c87b1246e47a92e2e47
ShivamAwasthi1129 <shivamawasthi1129@gmail.com>
2024-04-12 13:20:47 +0530

    addition of JW Tokens

---
+++ readme.txt
@@ -0,0 +1,2 @@
+ Execute the comand to run the code
+    command -> npm start   (make sure that this command is running through the currenct directory)
--- service/auth.js
+++ service/auth.js
@@ -1,14 +1,43 @@
-const sessionIdToUserMap = new Map();
+//stateless pattern
+const jwt = require("jsonwebtoken");
+const secret = "shivam$123@$";
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
   setUser,
   getUser,
 };
+
+//statefull pattern
+// const sessionIdToUserMap = new Map();
+
+// function setUser(id, user) {
+//   sessionIdToUserMap.set(id, user);
+// }
+
+// function getUser(id) {
+//   return sessionIdToUserMap.get(id);
+// }
+
+// module.exports = {
+//   setUser,
+//   getUser,
+// };
